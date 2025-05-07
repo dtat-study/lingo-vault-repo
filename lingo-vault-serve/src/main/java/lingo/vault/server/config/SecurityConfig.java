200375ab35e6d088a7cbb609095d958a71049f21
dtat-study <dtat.study@gmail.com>
2025-05-07 23:32:03 +0700

    chang fe domain

--- lingo-vault-serve/src/main/java/lingo/vault/server/config/SecurityConfig.java
+++ lingo-vault-serve/src/main/java/lingo/vault/server/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*")); // FE domain
+        config.setAllowedOrigins(List.of("https://lingolingo.vercel.app")); // FE domain
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type","Access-Control-Allow-Origin"));
         config.setAllowCredentials(true); // cho phép dùng cookie, authorization
